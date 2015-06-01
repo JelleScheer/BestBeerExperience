ca4fbfd76bbe47ecd01d64377acbd9e09e60b8ed
Barryvst <barry_skien@hotmail.com>
2015-06-01 12:13:14 +0200

    Druksensor kan uitgelezen worden
    Nu moet enkel nog gekeken worden hoe nauwkeurig de sensor is

---
+++ Druksensor fsr-406/Druksensor_fsr-406/Druksensor_fsr-406.ino
@@ -0,0 +1,21 @@
+// include library code:
+#include <LiquidCrystal.h>
+
+// Initializeer de lcd scherm met de juiste output porten
+// 8-rs
+// 9-e
+// 10~13 - d4~d7
+LiquidCrystal lcd(8,9,10,11,12,13);
+
+void setup() {
+// set up the LCD’s number of columns and rows:
+lcd.begin(16,2);
+// Print a message to the LCD.
+}
+void loop() {
+// set the cursor to column 0, line 1
+// (note: line 1 is the second row, since counting begins with 0):
+lcd.setCursor(0, 1);
+// print the number of seconds since reset:
+}
+
