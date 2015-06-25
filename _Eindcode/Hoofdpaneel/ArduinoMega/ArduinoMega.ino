cf02f91ccad88d1024c2bf099b4b4dd2ede9c9cc
Barryvst <Barryvst@gmail.com>
2015-06-25 12:25:32 +0200

    Git conflict fix

--- _Eindcode/Hoofdpaneel/ArduinoMega/ArduinoMega.ino
+++ _Eindcode/Hoofdpaneel/ArduinoMega/ArduinoMega.ino
@@ -16,6 +16,7 @@ RF24 radio(53, 48);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 int lastmsg = 1;
 String theMessage = "";
+boolean verstuurBierBestelling = false;
 
 // Initializeer de lcd scherm met de juiste output porten
 // 31-rs
@@ -24,7 +25,7 @@ String theMessage = "";
 LiquidCrystal lcd(31, 30, 32, 33, 34, 35);
 
 /* Radio pinnen Mega
- * CSN - 48
+ * CSM - 48
  * MI - 50
  * MO - 51
  * SCK - 52
@@ -45,16 +46,21 @@ void setup(void)
   // Init knoppen als input
   pinMode(2, INPUT);	   // Pin 2 is input to which a switch is connected = INT0
   pinMode(3, INPUT);	   // Pin 3 is input to which a switch is connected = INT0
+  pinMode(21, INPUT);        //Pin 21 is input to which a switch is connected = INT2
 
   //Interrupts toevoegen aan de knoppen
   attachInterrupt(0, systeemAan, RISING);
   attachInterrupt(1, systeemUit, RISING);
+  attachInterrupt(2, bierBestel, RISING);
 }
 
 void loop(void)
 {
-  if (radio.available())
-  {
+    if (radio.available())
+    {
+    radio.openReadingPipe(1, pipe);
+     radio.startListening();
+    
     bool done = false;
     done = radio.read(msg, 1);
     char theChar = msg[0];
@@ -85,7 +91,48 @@ void loop(void)
 
       theMessage = "";
     }
+    
+      if(verstuurBierBestelling == true)
+      {
+        radio.openWritingPipe(pipe);
+        sendStringToHoofdpaneel(String("go"));
+  
+        Serial.println("Writing!");
+  
+        verstuurBierBestelling = false;
+          
+        sendStringToHoofdpaneel(String("lon"));
+          
+        radio.openReadingPipe(1, pipe);
+        radio.startListening();  
+      }
+   }
+}
+
+void sendStringToHoofdpaneel(String message){
+
+  for (int i = 0; i < message.length(); i++)
+  {
+    int charToSend[1];
+    charToSend[0] = message.charAt(i);
+    radio.write(charToSend, 1);
+    Serial.print(message.charAt(i));
   }
+  Serial.println("");
+  
+  //send the 'terminate string' value...
+  msg[0] = 2;
+  radio.write(msg, 1);
+
+  /*delay sending for a short period of time.  radio.powerDown()/radio.powerupp
+  //with a delay in between have worked well for this purpose(just using delay seems to
+  //interrupt the transmission start). However, this method could still be improved
+  as I still get the first character 'cut-off' sometimes. I have a 'checksum' function
+  on the receiver to verify the message was successfully sent.
+  */
+  radio.powerDown();
+  delay(1000);
+  radio.powerUp();
 }
 
 void systeemAan() {
@@ -94,4 +141,8 @@ void systeemAan() {
 
 void systeemUit() {
   Serial.println("UIT");
+}
+
+void bierBestel() {
+    verstuurBierBestelling = true; 
 }
