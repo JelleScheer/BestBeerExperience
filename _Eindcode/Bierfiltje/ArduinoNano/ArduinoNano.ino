8f93a82951928fab1567a7fac6c77d99bf1a87e7
JelleScheer <jellescheer@gmail.com>
2015-06-25 06:35:25 -0700

    Commentaar toegevoegd aan hoofdpaneel en bierviltje

--- _Eindcode/Bierfiltje/ArduinoNano/ArduinoNano.ino
+++ _Eindcode/Bierfiltje/ArduinoNano/ArduinoNano.ino
@@ -41,53 +41,68 @@ int delayval = 1; // delay for half a second
  * IRQ - niet gebruikt
  */
 
+//Initialiseer radio
 int msg[1];
 RF24 radio(9, 10);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 
+//Boolean om tussen temperatuur en druk te wisselen
 boolean wisseldata = false;
 
 void setup(void)
 {
+  //Begin serial op baudrate 9600
   Serial.begin(9600);
+  
+  //Start radio op transmitting pipe
   radio.begin();
   radio.openWritingPipe(pipe);
 
-  // Start up the temperature library
+  // Start temperatuursensor
   sensors.begin();
   
-  // Start up RGBStrip
+  // Start rgbstrip
   pixels.begin();
 }
 
 void loop(void)
 {
+  //Wisselt constant tussen temperatuur en druk, en stuurt deze op naar het hoofdpaneel
   if(wisseldata){
-    sensors.requestTemperatures(); // Send the command to get temperatures
+    //Vraag temperatuur op
+    sensors.requestTemperatures();
+    //Stuur de temperatuur op met een "t" op het einde (als indicatie voor hoofdpaneel dat het om de temperatuur gaat)
     sendStringToHoofdpaneel(String(sensors.getTempCByIndex(0)) + "t");
   }
   else{
+    //Stuur de druk op met een "d" op het einde (als indicatie voor hoofdpaneel dat het om de druk gaat)
     sendStringToHoofdpaneel(String(analogRead(DRUKSENSORPINPUT)) + "d"); 
   }
   
-  // use gathered temperature to select the right colour
+  //Gebruik de opgehaalde temperatuur om op de RGBStrip de juiste kleur te laten zien
   for(int i=0;i<NUMPIXELS;i++)
   {
+    //Als de temperatuur onder de 10 graden is
     if(sensors.getTempCByIndex(0) < 10)
     {
-      pixels.setPixelColor(i, pixels.Color(101,211,245)); // Green
+      //Zet de RGBStrip op de kleur groen
+      pixels.setPixelColor(i, pixels.Color(101,211,245));
       pixels.show();
       delay(delayval);
     }
+    //Als de temperatuur tussen de 10 en 15 graden is
     else if(sensors.getTempCByIndex(0) > 10 && sensors.getTempCByIndex(0) < 15)
     {
-      pixels.setPixelColor(i, pixels.Color(229,144,32)); // Orange
+      //Zet de RGBStrip op de kleur oranje
+      pixels.setPixelColor(i, pixels.Color(229,144,32));
       pixels.show();
       delay(delayval);
     }
     else 
+    //Als de temperatuur hoger is dan 15 graden
     {
-      pixels.setPixelColor(i, pixels.Color(201,68,28)); // Red
+      //Zet de RGBStrip op de kleur rood
+      pixels.setPixelColor(i, pixels.Color(201,68,28));
       pixels.show();
       delay(delayval);
     }
@@ -97,7 +112,8 @@ void loop(void)
 }
 
 void sendStringToHoofdpaneel(String message){
-
+  
+  //Haal de message op die verstuurd moet worden, en converteer deze
   for (int i = 0; i < message.length(); i++)
   {
     int charToSend[1];
@@ -107,16 +123,11 @@ void sendStringToHoofdpaneel(String message){
   }
   Serial.println("");
   
-  //send the 'terminate string' value...
+  //Verstuur het bericht en stuur de "terminate string"
   msg[0] = 2;
   radio.write(msg, 1);
 
-  /*delay sending for a short period of time.  radio.powerDown()/radio.powerupp
-  //with a delay in between have worked well for this purpose(just using delay seems to
-  //interrupt the transmission start). However, this method could still be improved
-  as I still get the first character 'cut-off' sometimes. I have a 'checksum' function
-  on the receiver to verify the message was successfully sent.
-  */
+  //Zet radio even uit om noise te voorkomen
   radio.powerDown();
   delay(1000);
   radio.powerUp();
--- _Eindcode/Hoofdpaneel/ArduinoMega/ArduinoMega.ino
+++ _Eindcode/Hoofdpaneel/ArduinoMega/ArduinoMega.ino
@@ -4,19 +4,12 @@
 #include <SPI.h>
 #include <LiquidCrystal.h>
 
-/*
-This sketch receives strings from sending unit via nrf24
-and prints them out via serial.  The sketch waits until
-it receives a specific value (2 in this case), then it
-prints the complete message and clears the message buffer.
-*/
-
+//Initialiseer radio
 int msg[1];
 RF24 radio(53, 48);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 int lastmsg = 1;
 String theMessage = "";
-boolean verstuurBierBestelling = false;
 
 // Initializeer de lcd scherm met de juiste output porten
 // 31-rs
@@ -36,24 +29,29 @@ LiquidCrystal lcd(31, 30, 32, 33, 34, 35);
 
 //Bool systeem status
 boolean systeemIsAan = true;
-
 boolean lcdUitMessageDisplayed = false;
 
-
+//Bool bier bestelling bijhouden
+boolean verstuurBierBestelling = false;
 
 void setup(void)
 {
+  //Begin serial op baud rate 9600
   Serial.begin(9600);
+  
+  //Begin radio als receiving
   radio.begin();
   radio.openReadingPipe(1, pipe);
   radio.startListening();
+  
+  //Begin LCD scherm
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
 
   // Init knoppen als input
-  pinMode(2, INPUT_PULLUP);	   // Pin 2 is input to which a switch is connected = INT0
-  pinMode(3, INPUT_PULLUP);	   // Pin 3 is input to which a switch is connected = INT0
-  pinMode(21, INPUT_PULLUP);      //Pin 21 is input to which a switch is connected = INT2
+  pinMode(2, INPUT_PULLUP);	   // Pin 2 aanknop
+  pinMode(3, INPUT_PULLUP);	   // Pin 3 uitknop
+  pinMode(21, INPUT_PULLUP);      //Pin 21 bierbestelknop
 
   //Interrupts toevoegen aan de knoppen
   attachInterrupt(0, systeemAan, FALLING );
@@ -63,63 +61,89 @@ void setup(void)
 
 void loop(void)
 {
+  //Check of systeem aan staat
   if (systeemIsAan) {
+    //Als de uit-message op het LCD scherm stond, clear deze
     if (lcdUitMessageDisplayed) {
+      //Zet reading pipe open, mocht hij net uit de uitstand komen
+      radio.openReadingPipe(1, pipe);
+      radio.startListening();
+      
       lcdUitMessageDisplayed = false;
       lcd.clear();
     }
-
+    
+    //Als er een radio verbinding aanwezig is
     if (radio.available())
     {
+      //Open nogmaals de pipe als receiver (mocht hij in transmitting status staan)
       radio.openReadingPipe(1, pipe);
       radio.startListening();
 
+      //Haal de message op, zet deze in een string
       bool done = false;
       done = radio.read(msg, 1);
       char theChar = msg[0];
-
+      
+      //Initialisatie message
       if (msg[0] != 2)
       {
         theMessage.concat(theChar);
       }
+      //Geen initialisatie message
       else
       {
+        //Print de message in console
         Serial.println(theMessage);
+        
+        //Maak het LCD scherm schoon
         lcd.clear();
+        
+        //Als het bericht eindigt met een t
         if (theMessage.endsWith("t"))
         {
-
+          //Haal de T weg, print op het scherm Celcius: temperatuur
           theMessage.remove(theMessage.length() - 1);
           lcd.print("Celcius: " + theMessage);
         }
+        //Als het bericht eindigt met een d
         else if (theMessage.endsWith("d"))
         {
+          //Haal de D weg, print op het scherm Druk: druk
           theMessage.remove(theMessage.length() - 1);
           lcd.print("Druk: " + theMessage);
         }
+        //Als het een ander bericht is, print in console "ruis" voor debugging
         else
         {
-          lcd.print("Onbekende data, ruis?");
+          Serial.println("Ruis!");
         }
 
         theMessage = "";
       }
     }
+    //Als er op de bierbestelknop is gedrukt
     if (verstuurBierBestelling == true)
     {
+      //Switch de pipe naar transmitting
       radio.openWritingPipe(pipe);
 
-      Serial.println("Writing!");
-
+      Serial.println("Swtiching to transmitting. . .");
+      
+      //Reset bier bestel boolean
       verstuurBierBestelling = false;
-
+      
+      //Stuur bericht naar KeukenArduino dat bestelLED aan moet gaan
       sendStringToHoofdpaneel(String("lon"));
-
+      
+      //Switch de pipe weer terug naar receiving
       radio.openReadingPipe(1, pipe);
       radio.startListening();
     }
   }
+  //Als er op de UIT knop wordt gedrukt
   else {
+    //Laat standaard bericht op LCD scherm zien
     if (!lcdUitMessageDisplayed) {
       lcd.clear();
       lcd.print("Turn on for");
@@ -128,10 +152,9 @@ void loop(void)
 
       lcdUitMessageDisplayed = true;
       
+      //Open transmitting pipe en zend bericht naar KeukenArduino om het bestelLED uit te zetten (mocht hij nog aan staan)
       radio.openWritingPipe(pipe);
       sendStringToHoofdpaneel("suit");
-      radio.openReadingPipe(1, pipe);
-      radio.startListening();
     }
 
     delay(1000);
@@ -142,6 +165,7 @@ void loop(void)
 
 void sendStringToHoofdpaneel(String message) {
 
+  //Haal de message op die verstuurd moet worden, en converteer deze
   for (int i = 0; i < message.length(); i++)
   {
     int charToSend[1];
@@ -151,31 +175,27 @@ void sendStringToHoofdpaneel(String message) {
   }
   Serial.println("");
 
-  //send the 'terminate string' value...
+  //Verstuur het bericht en stuur de "terminate string"
   msg[0] = 2;
   radio.write(msg, 1);
 
-  /*delay sending for a short period of time.  radio.powerDown()/radio.powerupp
-  //with a delay in between have worked well for this purpose(just using delay seems to
-  //interrupt the transmission start). However, this method could still be improved
-  as I still get the first character 'cut-off' sometimes. I have a 'checksum' function
-  on the receiver to verify the message was successfully sent.
-  */
+  //Zet radio even uit om noise te voorkomen
   radio.powerDown();
   delay(1000);
   radio.powerUp();
 }
 
+//Interrupt van de aanknop
 void systeemAan() {
   systeemIsAan = true;
-
-  Serial.println("AAN");
 }
 
+//Interrupt van de uitknop
 void systeemUit() {
   systeemIsAan = false;
 }
 
+//Interrupt van de bierbestelknop
 void bierBestel() {
   verstuurBierBestelling = true;
 }
